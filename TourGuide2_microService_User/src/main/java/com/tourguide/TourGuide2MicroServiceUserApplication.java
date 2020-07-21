359ae201bf1cf0940b37e51f2a6893be0de63fcb
Jean-Benoit <jean.benoit.roussat@gmail.com>
2020-07-21 10:09:22 +0200

    ajout de zuul

--- TourGuide2_microService_User/src/main/java/com/tourguide/TourGuide2MicroServiceUserApplication.java
+++ TourGuide2_microService_User/src/main/java/com/tourguide/TourGuide2MicroServiceUserApplication.java
@@ -2,10 +2,12 @@ package com.tourguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.tourguide")
+@EnableDiscoveryClient
 public class TourGuide2MicroServiceUserApplication {
 
 	public static void main(String[] args) {
